eb86c65f5cd05982f6948a2cb87df05f7244a529
Cyrille Lebeaupin <clebeaupin@noop.fr>
2018-05-31 17:48:41 +0200

    Update language settings

--- src/vanilla/components/language-settings.ts
+++ src/vanilla/components/language-settings.ts
@@ -1,15 +1,56 @@
 import languageSettingsView from "ejs-loader!lib/vanilla/views/language-settings.ejs";
 
+import { PlayerEventType } from "lib/models/event";
 import { EventRegistry } from "lib/listeners/registry";
 import { AvpObject } from "lib/models/player";
+import { LanguageType } from "lib/models/language";
 
 import { BaseSettingsComponent } from "./base-settings";
-
-import { toggleElementAttribute, trapFocus, undoTrapFocus } from "lib/utils/dom";
-import { initPopin, togglePopin } from "lib/utils/popin";
 
 export class LanguageSettingsComponent extends BaseSettingsComponent {
     constructor(avp: AvpObject) {
         super(avp, languageSettingsView, "displaySettings");
+    }
+
+    public async postDomUpdate(rootElement: HTMLElement, domElements: any): Promise<any> {
+        await super.postDomUpdate(rootElement, domElements);
+        const playerElement = domElements["origin"]["root"];
+        const languageInputElements = document
+            .getElementsByName("language[" + rootElement.id + "]");
+
+        const applySettings = () => {
+            const selectedLanguage = this.avp.settingsManager.settings.language.type.toLocaleLowerCase();
+
+            Array.prototype.forEach.call(languageInputElements, (element: any) => {
+                element.checked = (element.value === selectedLanguage);
+            });
+        };
+
+        // Handlers
+        const refreshSettingsHandler = (event: any) => {
+            applySettings();
+        };
+
+        // Listeners
+        this.eventRegistry.register(
+            playerElement,
+            PlayerEventType.UiRefreshRequest,
+            refreshSettingsHandler
+        );
+
+        Array.prototype.forEach.call(languageInputElements, (element: any) => {
+            const languageChangeHandler = (event: any) => {
+                const newLanguage = event.target.value as string;
+                (this.avp.settingsManager.settings.language as any).type = newLanguage.toUpperCase();
+            };
+            this.eventRegistry.register(
+                element,
+                "change",
+                languageChangeHandler
+            );
+        });
+
+        // Update UI
+        applySettings();
     }
 }
